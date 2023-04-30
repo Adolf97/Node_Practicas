a27de02a32efddb291eec83a59c40a4c8a28d1da
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-04-30 13:46:13 -0600

    pruebas básicas con el servidor

--- servidor/app.js
+++ servidor/app.js
@@ -1,5 +1,15 @@
 const http = require('http')
+const port = 3000
 
 const servidor = http.createServer((req, res) => {
-  res.end('Hola Mundo!')
+  console.log('===> res (respuesta)')
+  
+  res.setHeader('content-type', 'application/json')
+  console.log(res.getHeaders())
+
+  res.end('Prueba')
+})
+
+servidor.listen(port, () => {
+  console.log(`The server is listening on http://localhost:${port}...`)
 })
